b7d7d436086d1d9584791843fed4e0bbc2203ec2
Skyll_Box <pinodamien@gmail.com>
2025-12-12 15:29:42 +0100

    Déplacement définitif du projet à la racine du dépôt

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/SpotyFusion/",
   plugins: [react(), basicSsl()],
   server: {
     port: 3000,
